efe3af765f19097795d1d7640f95affe26d3b443
salman5900 <salmanahamed5900@gmail.com>
2023-10-08 09:20:12 +0400

    new changes

--- src/index.js
+++ src/index.js
@@ -9,20 +9,26 @@ const book1 = {
   img: "./images/book-1.jpg",
 };
 
-// book 2
-
+// book2
 const book2 = {
   title: "ourth-Wing-Empyrean",
   author: "Rebecca-Yarros",
-  img: ".images.book-2.jpg",
+  img: "./images/book-2.jpg",
+};
+
+// book3
+const book3 = {
+  title: "Going-Infinite-Rise-Fall",
+  author: "Michael Lewis",
+  img: "./images/book-3.jpg",
 };
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book img={img} author={author} title={title} />
-      <Book img={img} author={author} title={title} />
-      <Book img={img} author={author} title={title} />
+      <Book img={book1.img} author={book1.author} title={book1.title} />
+      <Book img={book2.img} author={book2.author} title={book2.title} />
+      <Book img={book3.img} author={book3.author} title={book3.title} />
     </section>
   );
 }
@@ -32,7 +38,7 @@ function Book(props) {
     <>
       <article className="book">
         <img src={props.img} alt={props.title} />
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <h4
           style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
         >
